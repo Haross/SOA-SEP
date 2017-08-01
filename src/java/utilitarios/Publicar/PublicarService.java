7b08f7462956e4cc94f7d1b67d9d5c08677d8aa8
Josseline Arreola <Josseline Arreola@Josseline>
2017-07-31 20:38:29 -0500

    Publicar

--- src/java/utilitarios/Publicar/PublicarService.java
+++ src/java/utilitarios/Publicar/PublicarService.java
@@ -9,7 +9,9 @@ import BD.Conexion;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.jws.WebService;
+import org.netbeans.xml.schema.publicarschema.Capacitacion;
 import org.netbeans.xml.schema.publicarschema.Capacitaciones;
+import org.netbeans.xml.schema.publicarschema.Club;
 import org.netbeans.xml.schema.publicarschema.ClubDeLectura;
 
 /**
@@ -19,31 +21,29 @@ import org.netbeans.xml.schema.publicarschema.ClubDeLectura;
 @WebService(serviceName = "PublicarWSDLService", portName = "PublicarWSDLPort", endpointInterface = "org.netbeans.j2ee.wsdl.soa_sep.publicar.publicarwsdl.PublicarWSDLPortType", targetNamespace = "http://j2ee.netbeans.org/wsdl/SOA-SEP/Publicar/PublicarWSDL", wsdlLocation = "WEB-INF/wsdl/PublicarService/PublicarWSDL.wsdl")
 public class PublicarService {
     Conexion con = new Conexion();
-    public org.netbeans.xml.schema.publicarschema.Capacitaciones publicarCapacitacion(int tipo) {
+    
+    public org.netbeans.xml.schema.publicarschema.Capacitacion publicarCapacitacion(int tipo) {
         if (tipo==1){
-          
-        String query = "select * from capacitaciones;";
-        ResultSet rs = con.select(query);
-        try {
-            while(rs.next()){
+            String query = "select * from capacitaciones;";
+            ResultSet rs = con.select(query);
+            try {
                 Capacitaciones aux = new Capacitaciones();
-                aux.setFolio(rs.getString("folio"));
-                aux.setNombre(rs.getString("nombre"));
-                aux.setTipo(rs.getString("tipo"));
-                aux.setObjetivos(rs.getString("objetivos"));
-                aux.setRequisitos(rs.getString("requisitos"));
-                aux.setFechaPublicacion(rs.getString("fechapublicaciones"));
-            return aux;
-            
-         }
-         rs.close();
-        } catch (SQLException e) {
-            System.out.println("error: "+e);
-        }
-            
-        return null;  
-        }
-        else{
+                Capacitacion c = new Capacitacion();
+                while(rs.next()){
+                    aux.setFolio(rs.getString("folio"));
+                    aux.setNombre(rs.getString("nombre"));
+                    aux.setTipo(rs.getString("tipo"));
+                    aux.setObjetivos(rs.getString("objetivos"));
+                    aux.setRequisitos(rs.getString("requisitos"));
+                    aux.setFechaPublicacion(rs.getString("fechapublicacion"));
+                    c.getListaCapacitaciones().add(aux);
+             }
+             return c;
+            } catch (SQLException e) {
+                System.out.println("error: "+e);
+                return null;
+            }    
+        }else{
             if (tipo==2){
                 publicarClub(2);
             }
@@ -51,28 +51,30 @@ public class PublicarService {
         return null;
     }
 
-    public org.netbeans.xml.schema.publicarschema.ClubDeLectura publicarClub(int tipoClub) {
-       if (tipoClub==2){
-        String query = "select * from clublectura;";
-                ResultSet rs = con.select(query);
-                try {
-                    while(rs.next()){
-                        ClubDeLectura aux2 = new ClubDeLectura();
-                        aux2.setNombre(rs.getString("nombre"));
-                        aux2.setCupo(rs.getInt("cupo"));
-                        aux2.setTematica(rs.getString("tematica"));
-                    return aux2;
-                 }
-                 rs.close();
-                } catch (SQLException e) {
-                    System.out.println("error: "+e);
-                }
-        }else{
-           if (tipoClub==1){
-               publicarCapacitacion(1);
-           }
-       }
+    public org.netbeans.xml.schema.publicarschema.Club publicarClub(int tipoC) {
+        if (tipoC==2){
+            String query = "select * from clublectura;";
+            ResultSet rs = con.select(query);
+            try {
+                ClubDeLectura aux = new ClubDeLectura();
+                Club c = new Club();
+                while(rs.next()){
+                    aux.setNombre(rs.getString("nombre"));
+                    aux.setCupo(rs.getInt("cupo"));
+                    aux.setTematica(rs.getString("tematica"));
+                    c.getListaClub().add(aux);
+             }
+             return c;
+            } catch (SQLException e) {
+                System.out.println("error: "+e);
                 return null;
+            } 
+        }else{
+            if (tipoC==2){
+                publicarCapacitacion(2);
+            }
+        }
+        return null;
     }
     
 }
