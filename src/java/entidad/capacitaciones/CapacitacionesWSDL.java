19174686e37d771af5020b78a80c49ff7a292321
Jesus <jesusguillermomendez@gmail.com>
2017-08-02 22:51:11 -0500

    AgregadoCapacitaciones

--- src/java/entidad/capacitaciones/CapacitacionesWSDL.java
+++ src/java/entidad/capacitaciones/CapacitacionesWSDL.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Random;
 import javax.jws.WebService;
-import org.netbeans.xml.schema.capacitacionesschema.Capacitaciones;
+import org.netbeans.xml.schema.capacitacionschema.Capacitaciones;
 
 /**
  *
@@ -19,13 +19,13 @@ import org.netbeans.xml.schema.capacitacionesschema.Capacitaciones;
 @WebService(serviceName = "CapacitacionesWSDL", portName = "CapacitacionesWSDLPort", endpointInterface = "org.netbeans.j2ee.wsdl.soa_sep.capacitaciones.capacitacioneswsdl.CapacitacionesWSDLPortType", targetNamespace = "http://j2ee.netbeans.org/wsdl/SOA-SEP/capacitaciones/CapacitacionesWSDL", wsdlLocation = "WEB-INF/wsdl/CapacitacionesWSDL/CapacitacionesWSDL.wsdl")
 public class CapacitacionesWSDL {
 
-    public boolean capacitacionesWSDL(org.netbeans.xml.schema.capacitacionesschema.Capacitaciones registroC) {
-         Conexion con = new Conexion();
+    public boolean capacitacionesWSDL(org.netbeans.xml.schema.capacitacionschema.Capacitaciones datos) {
+        Conexion con = new Conexion();
         int folio=0;
         Random r = new Random();
         folio =r.nextInt(100000)+1;
         String Fol = String.valueOf(folio);
-        String query = "insert into capacitaciones VALUES ('"+Fol+"','"+registroC.getNombre()+"', '"+registroC.getTipo()+"','"+registroC.getObjetivos()+"','"+registroC.getRequisitos()+","+registroC.getFechapublicacion()+"')";
+        String query = "insert into capacitaciones VALUES ('"+Fol+"','"+datos.getNombre()+"', '"+datos.getTipo()+"','"+datos.getObjetivos()+"','"+datos.getRequisitos()+","+datos.getFechapublicacion()+"')";
         boolean estatus = con.insertar(query);
         return estatus;
     }
@@ -33,25 +33,31 @@ public class CapacitacionesWSDL {
     /**
      * Web service operation
      */
-    public Capacitaciones ConsultaWSDL(String Nombre) throws SQLException {
-         Conexion con = new Conexion();
+    public Capacitaciones ConsultaWSDL(String Nombre) {
+       Conexion con = new Conexion();
         String query = "select * from capacitaciones where nombre = '"+Nombre+"';";
         ResultSet rs = con.select(query);
-        while(rs.next()){
-            Capacitaciones aux = new Capacitaciones();
-            aux.setNombre(rs.getString("folio"));
-            aux.setNombre(rs.getString("nombre"));
-            aux.setNombre(rs.getString("tipo"));
-            aux.setNombre(rs.getString("objetivos"));
-            aux.setNombre(rs.getString("requisitos"));
-            aux.setNombre(rs.getString("fechapublicacion"));
-            
+        try {
+            while(rs.next()){
+                Capacitaciones aux = new Capacitaciones();
+                aux.setNombre(rs.getString("folio"));
+                aux.setNombre(rs.getString("nombre"));
+                aux.setNombre(rs.getString("tipo"));
+                aux.setNombre(rs.getString("objetivos"));
+                aux.setNombre(rs.getString("requisitos"));
+                aux.setNombre(rs.getString("fechapublicacion"));
+
             return aux;
             
+         }
+         rs.close();
+        } catch (SQLException e) {
+            System.out.println("error: "+e);
         }
-        rs.close();
             
         return null;
     }
+    
+    
     
 }
